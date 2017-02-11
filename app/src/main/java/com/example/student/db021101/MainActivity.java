919c8663632f343ee3f8069579c07c29265d1507
shaojung <shaojung@gmail.com>
2017-02-11 11:40:39 +0800

    use SAXParser

--- app/src/main/java/com/example/student/db021101/MainActivity.java
+++ app/src/main/java/com/example/student/db021101/MainActivity.java
@@ -4,19 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity {
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
+public class MainActivity extends AppCompatActivity {
+    MyDataHandler dataHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        dataHandler = new MyDataHandler();
         new Thread(){
             @Override
             public void run() {
@@ -37,7 +46,17 @@ public class MainActivity extends AppCompatActivity {
                     String str = result.toString("UTF-8");
                     Log.d("NET", str);
 
+                    SAXParserFactory spf = SAXParserFactory.newInstance();
+                    SAXParser sp = spf.newSAXParser();
+                    XMLReader xr = sp.getXMLReader();
+                    xr.setContentHandler(dataHandler);
+                    xr.parse(new InputSource(new StringReader(str)));
+
                 } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ParserConfigurationException e) {
+                    e.printStackTrace();
+                } catch (SAXException e) {
                     e.printStackTrace();
                 }
                 /*
---
+++ app/src/main/java/com/example/student/db021101/MyDataHandler.java
@@ -0,0 +1,31 @@
+package com.example.student.db021101;
+
+
+import android.util.Log;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * Created by student on 2017/2/11.
+ */
+
+public class MyDataHandler extends DefaultHandler {
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        super.startElement(uri, localName, qName, attributes);
+        Log.d("NET", qName);
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        super.endElement(uri, localName, qName);
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        super.characters(ch, start, length);
+    }
+}
